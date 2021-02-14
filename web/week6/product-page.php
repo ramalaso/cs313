b47ff996867449d34168e6cc3c5e271c59fd600e
Raul Laredo Soria <raul.laredo@jalasoft.com>
2021-02-13 23:09:20 -0400

    modified

--- web/week6/product-page.php
+++ web/week6/product-page.php
@@ -293,7 +293,7 @@ table tr th {
     $(function() {
         $('.delete').click(function() {
             var itemid = $(this).attr("delete_id");
-            var location = "product-page.php?action=edit&itemid=" + itemid;
+            var location = "product-page.php?action=delete&itemid=" + itemid;
             window.location.href = location;
         });
         $('.edit').click(function() {
