26a9a5a132efc991b284cd65692c15a295a92a4d
Raul Laredo Soria <raul.laredo@jalasoft.com>
2021-02-15 17:54:12 -0400

    Updated

--- web/week6/navbar.php
+++ web/week6/navbar.php
@@ -1,15 +1,15 @@
 <nav class="navbar navbar-inverse bg-inverse fixed-top bg-faded" style="font-family: 'Varela Round', sans-serif;">
     <div class="row">
         <div class="col navbar-shopping" style="width: 700px">
-            <a href="index.php" class="btn btn-info btn-header"><i style="color:white; align: right"
-                    class="fas fa-home pl-0"> Home</i></a>
-            <button id="clearcart" class="btn btn-danger btn-header"><i style="color:white; align: right"
-                    class="fas fa-trash pl-0"> Clear Cart</i></button>
+            <a href="index.php"><i style="color:white; align: right" class="fas fa-home fa-2x pl-0"></i><span
+                    style="color:white; text-align: center; font-size:1.5rem">Shopping LCG</span></a>
             <a href="./product-page.php" class="float-right"><i style="color:yellow;margin-left:'3px' "
-                    class="fas fa-dolly-flatbed pl-0">
-                    Manage Products</i></a>
-            <a href="shopping-cart.php" class="float-right"><i style="color:white; " class="fas fa-shopping-cart pl-0">
-                    Go to Cart</i></a>
+                    class="fas fa-dolly-flatbed fa-2x pl-0">
+                </i></a>
+            <a href="shopping-cart.php" class="float-right"><i style="color:white; margin:0 30px; "
+                    class="fas fa-shopping-cart fa-2x pl-0">
+                </i></a>
+
         </div>
     </div>
 </nav>
