8a16395fc95a9ee2ceccb3ba8c4d2484be6daa50
Raul Laredo Soria <raul.laredo@jalasoft.com>
2021-02-20 17:50:24 -0400

    Update

--- web/week6/login.php
+++ web/week6/login.php
@@ -34,7 +34,7 @@ switch ($action) {
         $clientData = $stmt->fetch(PDO::FETCH_ASSOC);
         // Compare the password just submitted against
         // the hashed password for the matching client
-        setcookie('clientdatapassword',  'ramalaso', strtotime('+1 year'), '/');
+        setcookie('clientdatapassword',  $clientData['clientPassword'], strtotime('+1 year'), '/');
         $hashCheck = password_verify($clientPassword, $clientData['clientPassword']);
         // $hashCheck = true;
         // If the hashes don't match create an error
